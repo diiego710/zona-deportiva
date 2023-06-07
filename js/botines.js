afcd904dcebc5afb855957e1ba6f7fb73c2f422c
Diego Sanchez <111304967+diiego710@users.noreply.github.com>
2023-06-07 18:03:02 -0300

    Subo a git hub

---
+++ js/botines.js
@@ -0,0 +1,179 @@
+const file = '../data/botines.json'
+const menuHam = document.querySelector('#menu-ham');
+const enlaces = document.querySelector('.nav');
+const contenidoProductos = document.querySelector('#productos-home');
+const ventanaCarrito = document.querySelector('#ventana-carrito');
+const carrito = document.querySelector('#carrito i');
+const totalCompra = document.querySelector('#total');
+const btnClose = document.querySelector('#close');
+const ventanaBody = document.querySelector('.ventana-body');
+let productosCarrito = [];
+
+class Producto{
+    constructor(imagen, nombre, precio,id){
+        this.imagen = imagen;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.id = id;
+        this.cantidad = 1;
+        this.subtotal = 0;
+    }
+    obtenerTotal(){
+        this.subtotal = this.precio * this.cantidad;
+    }
+}
+
+cargarEventos();
+
+function cargarEventos(){
+    document.addEventListener('DOMContentLoaded', () =>{
+        renderizarProductos();
+        productosCarrito = JSON.parse(localStorage.getItem('productLS')) || [];
+        mostrarProductosCarrito();
+    });
+
+    contenidoProductos.addEventListener('click', agregarProducto);
+    ventanaBody.addEventListener('click',eliminarProducto);
+    carrito.onclick = function(){
+        ventanaCarrito.style.display = 'block'
+    };
+
+    btnClose.onclick = function(){
+        ventanaCarrito.style.display = 'none'
+    };
+
+    window.onclick = function (event){
+        if(event.target == ventanaCarrito){
+            ventanaCarrito.style.display = 'none';
+        }
+    };
+
+    menuHam.addEventListener('click', () => {
+        enlaces.classList.toggle('activado');
+    });
+
+}
+
+function eliminarProducto(e){
+    if(e.target.classList.contains('eliminar-producto')){
+        const productoId = parseInt(e.target.getAttribute('id'));
+        
+        productosCarrito = productosCarrito.filter((producto) => producto.id !== productoId);
+        guardarProductosStorage();
+        mostrarProductosCarrito()
+        console.log(productosCarrito);
+    }
+}
+
+function agregarProducto(e){
+    e.preventDefault();
+
+    if(e.target.classList.contains('boton__carrito')){
+        const productoAgregado = e.target.parentElement;
+        
+        leerDatosProductos(productoAgregado);
+    };
+}
+
+function leerDatosProductos(producto){
+     const datosProducto = new Producto(
+        producto.querySelector('img').src,
+        producto.querySelector('h4').textContent,
+        Number(producto.querySelector('p').textContent.replace('$', '')),
+        parseInt(producto.querySelector('a').getAttribute('id')),
+     );
+     datosProducto.obtenerTotal;
+    agregarAlCarrito(datosProducto);
+}
+
+function agregarAlCarrito(productoAgregar){
+    const existeEnCarrito = productosCarrito.some((producto) => producto.id === productoAgregar.id);
+
+    if(existeEnCarrito){
+        const producto = productosCarrito.map((producto) => {
+            if(producto.id === productoAgregar.id){
+                producto.cantidad++;
+                producto.subtotal = producto.cantidad * producto.precio;
+
+                return producto;
+            }else{
+                return producto;
+            }
+        }); productosCarrito = producto;
+    } else{
+        productosCarrito.push(productoAgregar);
+    }
+    guardarProductosStorage();
+    mostrarProductosCarrito();
+}
+
+function mostrarProductosCarrito(){
+    limpiarCarrito();
+    
+    productosCarrito.forEach((producto) =>{
+        const {imagen, nombre, precio, cantidad, subtotal, id} = producto;
+
+        const div = document.createElement('div');
+        div.classList.add('contenedor-producto');
+        div.innerHTML = `
+        <img src="${imagen}" width="100">
+        <p>${nombre}</p>
+        <p>$${precio.toFixed(3)}</p>
+        <p>${cantidad}</p>
+        <p>$${subtotal.toFixed(3)}</p>
+        <a href="#" class="eliminar-producto" id="${id}"> X </a>
+        `;
+        ventanaBody.appendChild(div)
+    });
+
+    calcularTotal();
+}
+
+function calcularTotal(){
+    let total = productosCarrito.reduce((sumaTotal, producto) => sumaTotal + producto.subtotal, 0 );
+    totalCompra.innerHTML = `Total: $${total.toFixed(3)}`;
+}
+
+function limpiarCarrito(){
+    while(ventanaBody.firstChild){
+        ventanaBody.removeChild(ventanaBody.firstChild);
+    }
+}
+
+
+function guardarProductosStorage(){
+    localStorage.setItem('productLS',JSON.stringify(productosCarrito));
+}
+
+async function realizarPeticion(datos) {
+    try {
+        const response = await fetch(datos);
+
+        if (!response.ok) {
+            throw new Error(`Error en la petición: ${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+
+        return data;
+        } catch (error) {
+    }
+}
+
+
+
+async function renderizarProductos(){
+    const productos = await realizarPeticion(file);
+
+    productos.forEach((producto) =>{
+        const divCard = document.createElement('div');
+        divCard.classList.add('card')
+        divCard.innerHTML +=`
+            <img src="./img/${producto.img}" alt="${producto.nombre}" />
+            <h4>${producto.nombre}</h4>
+			<p>$${producto.precio.toLocaleString()}</p>
+			<a id=${producto.id} class="boton__carrito" href="#">Agregar</a>
+        `;
+        contenidoProductos.appendChild(divCard) 
+    });
+}
---
+++ js/productos.js
@@ -0,0 +1,76 @@
+const productos = [
+	{
+		id: 1,
+		nombre: 'Camiseta seleccion Argentina Tres Estrellas',
+		precio: 28000,
+		img: '../../img/camisetas/argentina2023.webp'
+	},
+	{
+		id: 2,
+		nombre: 'Camiseta Titular River Plate 2023',
+		precio: 22000,
+		img: '../../img/camisetas/river.webp'
+	},
+	{
+		id: 3,
+		nombre: 'Camiseta Titular Boca Juniors 2023',
+		precio: 22000,
+		img: '../../img/camisetas/boca.webp'
+	},
+	{
+		id: 4,
+		nombre: 'Camiseta Titular Real Madrid 2022-23',
+		precio: 22000,
+		img: '../../img/camisetas/realmadrid.webp'
+	},
+    {
+        id: 5,
+        nombre: 'Botines Nike Phantom Gx Club Fg Hombre',
+        precio:34999,
+        img: '../../img/botines/NIDD9483-446-1.jfif',
+    },
+
+    {
+        id: 6,
+        nombre: 'Botines adidas Predator Accuracy.3 Fg',
+        precio: 49999,
+        img: '../../img/botines/adidas-predator.jfif',
+    },
+
+	{
+		id: 7,
+		nombre: 'Botines Fútbol Puma Ultra Match Terreno Firme Hombre',
+		precio: 38000,
+		img: '../../img/botines/PU107438-01-1.jfif',
+	},
+    {
+        id: 8,
+        nombre: 'Botines Nike Phantom Gt2 Elite Fg',
+        precio: 134999,
+        img: '../../img/botines/NI_CZ9890-407-1.jfif',
+    },
+	{
+		id: 9,
+		nombre: 'Short River Plate Suplente',
+		precio: 5000,
+		img: 'short/river.webp'
+	},
+	{
+		id: 10,
+		nombre: 'Short Alemania 2022',
+		precio: 4500,
+		img: 'short/alemania.webp'
+	},
+	{
+		id: 11,
+		nombre: 'Short Colombia 2023',
+		precio: 4500,
+		img: 'short/colombia.jfif'
+	},
+	{
+		id: 12,
+		nombre: 'Short Real Madrid 2022-23',
+		precio: 5500,
+		img: 'short/realmadrid.jfif'
+	},
+];
